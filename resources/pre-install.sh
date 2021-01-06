8649e9404e098c8e8675c9f54c1da8828234c86e
JoeWaite <35706754+JoeWaite@users.noreply.github.com>
2021-01-06 20:21:43 +0000

    Update pre-install.sh

--- resources/pre-install.sh
+++ resources/pre-install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 #upgrade the packages
-apt-get update && apt-get upgrade -y --force-yes
+apt-get update && apt-get upgrade -y
 
 #install packages
-apt-get install -y --force-yes git
+apt-get install -y git
 
 #get the install script
 cd /usr/src && git clone https://github.com/BinkNetworks/JSFSInstall
-
-#change the working directory
-cd /usr/src/JSFSInstall
